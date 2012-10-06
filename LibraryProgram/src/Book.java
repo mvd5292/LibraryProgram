69117f7614fca697ee2b4d3f7b2b71facd69b07a
Zach Kehs <zik5024@psu.edu>
2012-10-06 18:53:53 -0400

    book finished, super simple

--- LibraryProgram/src/Book.java
+++ LibraryProgram/src/Book.java
@@ -1,5 +1,17 @@
-
+//Book Class
+//Zach kehs
 public class Book
 {
+	public String title;
+	public String author;
+	public String holder;
+	public String subject;
 	
+	public int pagecount;
+	
+	public boolean onHold;
+	
+	public int dateDue; //we originally thought this was going to be a function, but a public int will work better
 }
+
+//No functions, so we don't need a test driver.
